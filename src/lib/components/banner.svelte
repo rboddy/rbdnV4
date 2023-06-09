8c700069055a9cb33e5c143c290b02432b1491d3
rboddy <avidryanb@gmail.com>
2023-06-09 17:36:20 -0400

    Getting this started

---
+++ src/lib/components/banner.svelte
@@ -0,0 +1,19 @@
+<div class="banner-container">
+    <h1>Hello World</h1>
+</div>
+
+<style>
+    .banner-container {
+        background: url("/trianglify-export.png");
+        width: 100%;
+        height: 80vh;
+        display: flex;
+        align-items: end;
+        padding: 0px 5vmax;
+        box-sizing: border-box;
+    }
+    h1 {
+        font-size: 80px;
+        color: var(--text);
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<slot></slot>
+
+<style>
+    :global(:root){
+        --text: #00031F;
+        --primary: #0B5CFF;
+        --secondary: #00EDE7;
+    }
+    :global(body){
+        margin: 0;
+        font-family: 'Roboto', sans-serif;
+    }
+    :global(h1, h2, h3){
+        font-family: 'Manrope', sans-serif;
+        margin: 0;
+    }
+
+</style>
